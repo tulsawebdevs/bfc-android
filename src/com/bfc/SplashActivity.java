172cef5aa6191e094c7f0af8d53f947030f66758
blixa morgan <blixa@morgancraft.net>
2011-10-08 00:45:41 -0500

    Added Search Tab layout and activity
    Signed-off-by: blixa morgan <blixa@morgancraft.net>

--- src/com/bfc/SplashActivity.java
+++ src/com/bfc/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.bfc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,17 @@ public class SplashActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+
+			public void run() {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(i);
+				finish();
+			}
+        	
+        }, 5000);
     }
 }
