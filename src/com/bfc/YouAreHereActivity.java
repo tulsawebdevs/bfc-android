7e9451d6a38226dc4500ff9f5b0873fa92c089fd
blixa morgan <blixa@morgancraft.net>
2011-10-07 23:27:29 -0500

    updated splash layout added YouAreHere layout and activity
    Signed-off-by: blixa morgan <blixa@morgancraft.net>

---
+++ src/com/bfc/YouAreHereActivity.java
@@ -0,0 +1,13 @@
+package com.bfc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YouAreHereActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.you_are_here);
+    }
+}
