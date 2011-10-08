dd12bde52a30b06613317b54a609628a8f00351f
Darren Gibson <zarthross@gmail.com>
2011-10-08 00:33:32 -0500

    Changed MainActivity to a TabActivity. Changed main layout to a tab layout

--- src/com/bfc/MainActivity.java
+++ src/com/bfc/MainActivity.java
@@ -1,13 +1,27 @@
 package com.bfc;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        TabHost tabHost= getTabHost();
+        TabHost.TabSpec spec;
+        Intent intent;
+        
+        intent=new Intent().setClass(this,YouAreHereActivity.class);
+        spec=tabHost.newTabSpec("youAreHere")
+        			.setIndicator("Here")
+        			.setContent(intent);
+        tabHost.addTab(spec);
+
+        
     }
 }
