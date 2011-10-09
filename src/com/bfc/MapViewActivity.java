bb8d74c41cc323d31b796187bc073b318061f17d
blixa morgan <blixa@morgancraft.net>
2011-10-08 19:44:00 -0500

    Updated MapViewActivity.java to use json draw Untested
    Signed-off-by: blixa morgan <blixa@morgancraft.net>

--- src/com/bfc/MapViewActivity.java
+++ src/com/bfc/MapViewActivity.java
@@ -3,6 +3,10 @@ package com.bfc;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
@@ -15,17 +19,20 @@ import com.google.android.maps.OverlayItem;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MapViewActivity extends MapActivity {
 	private MapView myMapView;
 	private MapController myMapController;
 	private com.google.android.maps.MyLocationOverlay mlol;
-
+    private Locations itemizedoverlay;
+    private List<Overlay>  list;
 	private void CenterLocation(GeoPoint centerGeoPoint) {
 		myMapController.animateTo(centerGeoPoint);
 	};
-
+	Handler page;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,23 +45,34 @@ public class MapViewActivity extends MapActivity {
 		// myLocationManager = (LocationManager)
 		// getSystemService(Context.LOCATION_SERVICE);
 		// myLocationListener = new MyLocationListener();
-		List<Overlay> list = myMapView.getOverlays();
+		list = myMapView.getOverlays();
 		list.add(mlol);
 		// myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-		// 0, 0, myLocationListener);
+		// 0, 0, myLocationListener);\
+		page = new Handler();
+		
 		mlol.runOnFirstFix(new Runnable() {
 			public void run() {
+				page.post(new Runnable(){
+					public void run() {
+
+						int lat = mlol.getMyLocation().getLatitudeE6();
+						int lng = mlol.getMyLocation().getLongitudeE6();
+						new GetPage().execute(lat,lng);
+					};
+				});
 				CenterLocation(mlol.getMyLocation());
 			}
 		});
 		Drawable marker = getResources().getDrawable(
 				android.R.drawable.ic_menu_myplaces);
-		Locations itemizedoverlay = new Locations(marker, this);
+		itemizedoverlay = new Locations(marker, this);
 		GeoPoint point = new GeoPoint(19240000, -99120000);
 		OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!",
 				"I'm in Mexico City!");
 		itemizedoverlay.addOverlay(overlayitem);
 		list.add(itemizedoverlay);
+		
 	}
 
 	@Override
@@ -140,5 +158,36 @@ public class MapViewActivity extends MapActivity {
 			return true;
 		}
 	}
+	private class GetPage extends AsyncTask<Integer, Void, JSONObject> {
+	     protected JSONObject doInBackground(Integer... cords) {
+	    	 double lat = cords[0]/1000000;
+	    	 double lng = cords[1]/1000000;
+	    	 JSONObject jo=null;
+	    	 jo = HttpHandler.getPage(lat, lng, 25, 1);
+	    	 return jo;
+	     }
+
+	     protected void onPostExecute(JSONObject jo) {
+	    	 try {
+	    		JSONArray result = jo.getJSONArray("Photos");
+	    		for(int x=0;x>24;x++){
+	    		JSONObject obs = result.getJSONObject(x);
+	    		double lat = obs.getDouble("latitude");
+				double lng = obs.getDouble("longitude");
+				String title = obs.getString("title");
+				String subject = obs.getString("subject");
+				GeoPoint point = new GeoPoint((int) lat*1000000, (int) lng*1000000);
+				OverlayItem overlayitem = new OverlayItem(point, title,
+						subject);
+				itemizedoverlay.addOverlay(overlayitem);
+				list.add(itemizedoverlay);
+	    		}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	 
+	     }
+	 }
 
 }
