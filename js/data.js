4e6b37ffba07f6f582323a7c93695888c188cc26
Blixa Morgan <blixa@projectmakeit.com>
2012-10-13 14:58:47 -0500

    changed data loading

--- js/data.js
+++ js/data.js
@@ -1,2 +1,33 @@
-( function($) {
+var jsondata
+$(function() {
+	$.get('./data/myDatabase.sql', function(response) {
+  		// console.log("got db dump!", response);
+		init=false
+  		var db = openDatabase('myDatabase', '1.0', 'myDatabase', 10000000, function(){init=true});
+		if(init){
+  			processQuery(db, 2, response.split(';\n'), 'myDatabase'); 
+		}
+	});
+
 });
+function processQuery(db, i, queries, dbname) {
+    if(i < queries.length -1) {
+      console.log(i +' of '+queries.length);
+      if(!queries[i+1].match(/(INSERT|CREATE|DROP|PRAGMA|BEGIN|COMMIT)/)) {
+        queries[i+1] = queries[i]+ ';\n' + queries[i+1];
+         return processQuery(db, i+1, queries, dbname);
+      }
+      console.log('------------>', queries[i]);
+      db.transaction( function (query){ 
+        query.executeSql(queries[i]+';', [], function(tx, result) {
+          processQuery(db, i +1, queries,dbname);  
+        });          
+      }, function(err) { 
+      console.log("Query error in ", queries[i], err.message);                          
+      processQuery(db, i +1, queries, dbname);   
+      });
+  } else {
+      console.log("Done importing!");
+  }
+}
+
