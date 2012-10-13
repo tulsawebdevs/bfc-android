1358f1e3ec444651f62317c8859cf0b11224dab5
Blixa Morgan <blixa@projectmakeit.com>
2012-10-13 15:56:29 -0500

    fixed first load of the db in data.js

--- js/data.js
+++ js/data.js
@@ -1,12 +1,24 @@
-var jsondata
 $(function() {
+	var load=false;
   	var db = openDatabase('myDatabase', '1.0', 'myDatabase', 10000000);
-	$.ajax('./data/myDatabase.sql', {
-		success:function(response) {
-  			// console.log("got db dump!", response);
-  			processQuery(db, 2, response.split(';\n'), 'myDatabase')}
+	db.transaction(function (tx) {
+		tx.executeSql('SELECT Z_VERSION FROM Z_METADATA', [], function (tx, results) {
+			console.log(results);
+			if(results.rows.length>0){
+				console.log('works');
+				load=false;
+			}
 		});
-
+	});
+	if(load){
+		console.log('loading')
+		$.ajax('./data/myDatabase.sql', {
+			success:function(response) {
+	  			// console.log("got db dump!", response);
+	  			processQuery(db, 2, response.split(';\n'), 'myDatabase')
+				}
+			});
+	}
 });
 function processQuery(db, i, queries, dbname) {
     if(i < queries.length -1) {
@@ -15,7 +27,7 @@ function processQuery(db, i, queries, dbname) {
         queries[i+1] = queries[i]+ ';\n' + queries[i+1];
          return processQuery(db, i+1, queries, dbname);
       }
-      console.log('------------>', queries[i]);
+     console.log('------------>', queries[i]);
       db.transaction( function (query){ 
         query.executeSql(queries[i]+';', [], function(tx, result) {
           processQuery(db, i +1, queries,dbname);  
