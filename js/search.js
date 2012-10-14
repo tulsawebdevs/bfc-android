7640faa3de9dad44cd27ce9cba9b9c58dc1a3056
Blixa Morgan <blixa@projectmakeit.com>
2012-10-13 19:49:44 -0500

    clicking on search results pops up image

--- js/search.js
+++ js/search.js
@@ -8,9 +8,8 @@ $(function(){
               for (i=0;i<results.rows.length;i++){
                 item = results.rows.item(i);
                 $('#search-results').append(
-                  "<li>"+item.ZTITLE+" "+
-                  item.ZDESC+" "+
-                  "</li>"
+                  "<li><a href='#photopopup' data-rel='popup' onClick='popupImage("+item.ZCONTENTDMNUMBER+")'>"+item.ZTITLE+" "+
+                  item.ZDESC+" "+"</a></li>"
                 );
               }
               $('#search-results').listview('refresh');
@@ -18,6 +17,15 @@ $(function(){
 		});
 		return false;
 	});
+	$( "#photopopup" ).on({
+        popupbeforeposition: function() {
+            var maxHeight = $( window ).height() - 60 + "px";
+            $( "#photopopup img" ).css( "max-height", maxHeight );
+        }
+    });
 });
+function popupImage(idNum){
+	$( "#photopopup img" ).attr('src','images/'+idNum+'.jpg');
+}
 
 
