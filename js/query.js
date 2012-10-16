19e47f2ddcfc727de731734bb6ed06dc16c7058e
Darren Gibson <zarthross@gmail.com>
2012-10-16 13:27:22 -0500

    Added mustahce templates. Added popup for nearyou page

--- js/query.js
+++ js/query.js
@@ -1,6 +1,4 @@
 
-function placesnear(lat,lng,callback){
-
 function distance(lat1,lon1,lat2,lon2) {
   var R = 6371; // km (change this constant to get miles)
   var dLat = (lat2-lat1) * Math.PI / 180;
@@ -13,9 +11,11 @@ function distance(lat1,lon1,lat2,lon2) {
   return d;
 }
 
+
+function placesnear(lat,lng,callback){
   var db = openDatabase('myDatabase','1.0','myDatabase',10000000);
   db.transaction(function(tx){
-    tx.executeSql('select ZCOORD_LAT as lat, ZCOORD_LONG as lng, ZTITLE as title, ZDESC as desc from ZPHOTO',[], 
+    tx.executeSql('select ZCONTENTDMNUMBER, ZCOORD_LAT as lat, ZCOORD_LONG as lng, ZTITLE, ZDESC from ZPHOTO',[], 
     function(tx,results){
       var len = results.rows.length,i;
       var data=[]
@@ -31,23 +31,24 @@ function distance(lat1,lon1,lat2,lon2) {
   });
 }
 
-
-function populateNearYou(tag){
-        navigator.geolocation.getCurrentPosition(
-        function(position){
-          placesnear(position.coords.latitude,position.coords.longitude,
-            function(items){
-              var i;
-              console.log("Length: "+items.length);
-              for (i=0;i<items.length;i++){
-                item = items[i];
-                $(tag).append(
-                  "<li>"+item.title+" "+
-                  item.distance+" "+
-                  "</li>"
-                );
+function populateNearYou(tag,imageTag){
+  navigator.geolocation.getCurrentPosition(
+    function(position){
+      placesnear(position.coords.latitude,position.coords.longitude,
+        function(data){
+          //var i;
+          console.log("Length: "+data.length);
+          $.get("listitem.html",function(template) {
+            var t = $.mustache(template,{content:data});
+            $(tag).append(t);
+            $(tag+" ul").listview();
+            $( imageTag ).on({
+              popupbeforeposition: function() {
+                var maxHeight = $( window ).height() - 60 + "px";
+                $( imageTag ).css( "max-height", maxHeight );
               }
-              $(tag).listview('refresh');
-        })
-      })
+            });
+          });
+    })
+  })
 }
