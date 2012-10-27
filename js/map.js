11fa0147988056d8deb4799b345a4858c71961b4
Darren Gibson <zarthross@gmail.com>
2012-10-27 10:53:18 -0500

    fixed issue with map centering.  Now the location is always correct, even if its not centered on tulsa.

--- js/map.js
+++ js/map.js
@@ -1,20 +1,21 @@
 function loadmap(){
-  var map = $('#map');
-  navigator.geolocation.getCurrentPosition(function(position) {
-    //alert('itworked');
-    latlong = position.coords.latitude+','+ position.coords.longitude
-    //latlong =google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-    map = map.gmap({'center':latlong, 'zoom':15});
-    //$('#map').gmap('addMarker', {'position':position.coords.latitude+','+position.coords.longitude});
-  });
+  $('#map').gmap().bind('init',function(event,map){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      //alert('itworked');
+      var latlong = new google.maps.LatLng(position.coords.latitude, position.coords.longitude,true);
+      $('#map').gmap('option','center',latlong);
+      $('#map').gmap('option','zoom',15);
+      $('#map').gmap('refresh');
+    });
 
-  if(map.gmap('get','markers').length != data.length){
-    map.gmap('clear','markers');
-    var len = data.length;
-    for (i = 0; i < len; i++) {
-      map.gmap('addMarker', {'position':data[i].LAT+','+data[i].LONG}).click((function(index){return function()  {
-        map.gmap('openInfoWindow', { 'content': data[index].VISUAL_DESCRIPTION + "<img src='images/"+data[index].CONTENTDM+".jpg' style='width:100px;height:100px;'></img>" }, this);
-      }})(i)); 
+    if($('#map').gmap('get','markers').length != data.length){
+      $('#map').gmap('clear','markers');
+      var len = data.length;
+      for (i = 0; i < len; i++) {
+        $('#map').gmap('addMarker',{'position':data[i].LAT+','+data[i].LONG}).click((function(index){return function()  {
+          $('#map').gmap('openInfoWindow',{ 'content': data[index].VISUAL_DESCRIPTION + "<img src='images/"+data[index].CONTENTDM+".jpg' style='width:100px;height:100px;'></img>" }, this);
+        }})(i)); 
+      }
     }
-  }
+  });
 }
