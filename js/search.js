c2ee9afe8efbb0bb623e8626d72abc163791c639
Darren Gibson <zarthross@gmail.com>
2012-10-27 09:01:34 -0500

    fixed the search page after last merge

--- js/search.js
+++ js/search.js
@@ -5,19 +5,26 @@ $(function(){
       function(position){
         var lat = position.coords.latitude;
         var lng = position.coords.longitude;
-        var len = results.rows.length,i;
-        var data=[]
+        var srch = $('#search-basic').val().toLowerCase();
+        var data1 = _.filter(data,function(item){
+          return item.NAME.toLowerCase().indexOf(srch) != -1 ||
+                 item.VISUAL_DESCRIPTION.toLowerCase().indexOf(srch) !=-1 ||
+                 item.ADDRESS.toLowerCase().indexOf(srch) != -1 ||
+                 item.TAGS.toLowerCase().indexOf(srch) !=-1;
+        });
+        var len = data1.length;
         for (i=0;i<len;i++){
-          var d = results.rows.item(i);
-          d.distance = distance(d.ZCOORD_LAT,d.ZCOORD_LONG,lat,lng);
+          var d = data1[i];
+          d.distance = distance(d.LAT,d.LONG,lat,lng);
           if(d.distance<0){
             d.distance_format = (d.distance*1000).toFixed(1) + "m";
           }else{
             d.distance_format = d.distance.toFixed(2) + "km";
           }
-          data.push(d);
+          data1[i]=d;
         }
-        listViewTemplate("#search-results","#Search div #photopopup img",data,"listitem.html");
+        data = _.sortBy(data,function(d){return d.distance});
+        listViewTemplate("#search-results","#Search div #photopopup img",data1,"listitem.html");
     });
     return false;
   });
